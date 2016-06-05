bf889297525892b2e755d358d5b5e69185e6e290
Иван Недзвецкий <stels-cs@yandex.ru>
2016-06-05 15:58:15 +0300

    Landing

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+var webserver = require('gulp-webserver');
+ 
+gulp.task('webserver', function() {
+  gulp.src('.')
+    .pipe(webserver({
+        fallback:   'index.html',
+        livereload: true,
+        directoryListing: true,
+        open: true
+    }));
+});
